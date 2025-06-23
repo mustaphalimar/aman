4a44b833cbb536353400e632ebd2fa9bcf16446b
Hamza <Hamza@ELBADAOUI>
2025-06-23 13:32:31 +0100

    sss

--- mobile/Services/apiClient.js
+++ mobile/Services/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL: 'http://192.168.217.207:8080/api', 
+  baseURL: 'http://192.168.4.207:8080/api', 
   //baseURL: 'https://aman-production-e7eb.up.railway.app/api', 
 });
 
